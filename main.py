0c05b426e7c75712eb6941c7c5f8432cac02fb73
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2025-06-01 21:45:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import torch
+import torch import torch .nn as nn
+import torch.nn.functional as F
+import math 
+
+class MultiHeadAttention(nn.Module):
+  def __init__(self, d-model, num_heads):
+    super.().__init__()
+    self.d_model = d_model
+    self.num_heads = num_heads
+    self.head_dim =d = 
+    
 
